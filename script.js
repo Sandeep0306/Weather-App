f904435da5ad658031f5ffe998c393d33e2842fb
Sandeep Biswal <112690853+Sandeep0306@users.noreply.github.com>
2023-02-26 22:58:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,16 +39,88 @@ let weather = {
     },  
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
-
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
+// fetching data for geolocation using API
+let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+      var apikey = '6a31ae2609604a239e74f16f9c9d519b';
+  
+      var api_url = "https://api.opencagedata.com/geocode/v1/json";
+  
+      var request_url =
+        api_url +
+        "?" +
+        "key=" +
+        apikey +
+        "&q=" +
+        encodeURIComponent(latitude + "," + longitude) +
+        "&pretty=1" +
+        "&no_annotations=1";
+  
+      var request = new XMLHttpRequest();
+      request.open("GET", request_url, true);
+  
+      request.onload = function () {
+  
+        if (request.status == 200) {
+          var data = JSON.parse(request.responseText);
+          weather.fetchWeather(data.results[0].components.state);
+          console.log(data.results[0].components.state);
+        }
+        else if(request.status == 200) {
+            var data = JSON.parse(request.responseText);
+            weather.fetchWeather(data.results[0].components.city);
+            console.log(data.results[0].components.state);
+          }
+        
+        else if (request.status <= 500) {
+  
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log("error msg: " + data.status.message);
+        } else {
+          console.log("server error");
+        }
+      };
+  
+      request.onerror = function () {
+        console.log("unable to connect to server");
+      };
+  
+      request.send(); 
+    },
+    getLocation: function() {
+      function success (data) {
+        geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+      }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, console.error);
+      }
+      else {
+        weather.fetchWeather("New Delhi");
+      }
     }
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
   });
-
-weather.fetchWeather("New Delhi");
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("New Delhi");
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  geocode.getLocation();
